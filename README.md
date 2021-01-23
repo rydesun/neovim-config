274f075486a3854025ceac90fda8080414c0859a
rydesun <rydesun@gmail.com>
2021-01-23 23:10:39 +0800

    Rewrite install script

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 My NeoVim configuration files, using [vim-plug](https://github.com/junegunn/vim-plug)
 to manage plugins.
+
+## Installation
+
+```shell
+git clone https://github.com/rydesun/neovim-config.git ~/.config/nvim
+~/.config/nvim/tools/install.sh
+```
--- tools/install.sh
+++ tools/install.sh
@@ -1,31 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
 msg() {
 	echo "$@" >&2
 }
-die() {
-	msg "$@"
-	exit 1
-}
 
-conf=$1
-confdir=${XDG_CONFIG_HOME:-"${HOME}/.config"}/nvim
 datadir=${XDG_DATA_HOME:-"${HOME}/.local/share"}/nvim
 
-if [[ ! -f $conf ]]; then
-	die "wrong init.vim"
-fi
-
-mkdir -p "${confdir}"
-# backup file will be generated
-cp "${conf}" "${confdir}/init.vim" -S .bak -v >&2
-
-msg "Download plugin manager..."
-curl -fLo "${datadir}/site/autoload/plug.vim" --create-dirs --silent \
+msg "Downloading plugin manager..."
+curl -fLo "${datadir}/site/autoload/plug.vim" --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-msg "Install plugin..."
-msg "===== nvim output ====="
-nvim --headless +PlugClean +PlugInstall +qa
-msg "======================="
-msg "Finish."
+msg "Installing plugins..."
+nvim --headless +PlugInstall +qa
