71dc8e05eda23e8ce012c2f9b6274d5b7f18f138
rydesun <rydesun@gmail.com>
2021-07-29 09:27:40 +0800

    Fix count plugin

--- lua/count.lua
+++ lua/count.lua
@@ -1,7 +1,5 @@
 local M = {
-	zh_range = '[\\u3400-\\u4DB5\\u4E00-\\u9FEA'..
-	'\\uFA0E\\uFA0F\\uFA11\\uFA13\\uFA14\\uFA1F'..
-	'\\uFA21\\uFA23\\uFA24\\uFA27-\\uFA29]',
+	zh_range = '[\\u3400-\\u4DBF\\u4E00-\\u9FFC]'
 }
 
 function M:cmd_count_zh()
