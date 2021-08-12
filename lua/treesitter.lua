e661d4691a12cdbbb6f35bde4c6847086f2e02bf
rydesun <rydesun@gmail.com>
2021-08-12 23:33:20 +0800

    Update treesitter config

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -15,11 +15,21 @@ require'nvim-treesitter.configs'.setup {
   textobjects = {
     select = {
       enable = true,
+      lookahead = true,
       keymaps = {
         ["af"] = "@function.outer",
         ["if"] = "@function.inner",
         ["ac"] = "@class.outer",
         ["ic"] = "@class.inner",
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>]p"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>[p"] = "@parameter.inner",
       },
     },
     move = {
