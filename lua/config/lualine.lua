e3da85208e9226d594872edcb740b51f22e4581b
rydesun <rydesun@gmail.com>
2022-05-20 17:54:08 +0800

    Update lualine filename

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -23,6 +23,30 @@ local function encoding()
   return fenc == 'utf-8' and '' or fenc
 end
 
+local filename = require('lualine.components.filename'):extend()
+local highlight = require'lualine.highlight'
+
+function filename:init(options)
+  filename.super.init(self, options)
+  self.status_colors = {
+    cwd = highlight.create_component_highlight_group(
+      {fg = '#5f6d67'}, 'filename_cwd', self.options),
+  }
+end
+
+function filename:update_status()
+  local default_highlight = self:get_default_hl()
+  local path = filename.super.update_status(self)
+  local parent = vim.fn.getcwd() .. '/'
+  if path:find(parent, 1, true) == 1 then
+    local path_left = highlight.component_format_highlight(
+      self.status_colors.cwd) .. parent .. default_highlight
+    local path_right = path:sub(#parent+1)
+    path =  path_left .. path_right
+  end
+  return path
+end
+
 require'lualine'.setup {
   options = {
     theme = 'everforest',
@@ -39,8 +63,8 @@ require'lualine'.setup {
     },
     lualine_c = {
       {'fileformat', symbols = {unix = ''}, padding = {left = 1, right = 0}},
-      {'filename',
-        path = 3, symbols = {modified = '  ', readonly = ' ', unnamed = ''}},
+      {filename,
+        path = 2, symbols = {modified = '  ', readonly = ' ', unnamed = ''}},
     },
     lualine_x = {encoding},
     lualine_y = {'diagnostics'},
