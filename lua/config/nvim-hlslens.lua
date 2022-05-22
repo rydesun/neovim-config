8126832f882f7b715f62b5161a454a30840f31eb
rydesun <rydesun@gmail.com>
2022-05-23 02:23:26 +0800

    Merge vmlens config into hlslens

--- lua/config/nvim-hlslens.lua
+++ lua/config/nvim-hlslens.lua
@@ -1,5 +1,9 @@
 require'hlslens'.setup{nearest_only = true}
 
+local hlslens = require'hlslens'
+local config = require'hlslens.config'
+local lensBak
+
 local keymap = vim.api.nvim_set_keymap
 local kopts = {noremap = true, silent = true}
 keymap('n', 'n',
@@ -13,11 +17,42 @@ keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
 keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
 keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
 
+-- https://github.com/kevinhwang91/nvim-hlslens#integrate-with-other-plugins
+local function override_lens(render, posList, nearest, idx, relIdx)
+    local _ = relIdx
+    local lnum, col = unpack(posList[idx])
+
+    local text, chunks
+    if nearest then
+        text = ('[%d/%d]'):format(idx, #posList)
+        chunks = {{' ', 'Ignore'}, {text, 'VM_Extend'}}
+    else
+        text = ('[%d]'):format(idx)
+        chunks = {{' ', 'Ignore'}, {text, 'HlSearchLens'}}
+    end
+    render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+end
+
+local function vmlens_start()
+    if hlslens then
+        lensBak = config.override_lens
+        config.override_lens = override_lens
+        hlslens.start(true)
+    end
+end
+
+local function vmlens_exit()
+    if hlslens then
+        config.override_lens = lensBak
+        hlslens.start(true)
+    end
+end
+
 vim.api.nvim_create_autocmd({"User"}, {
   pattern = {"visual_multi_start"},
-  callback = function() require'config/vmlens'.start() end
+  callback = function() vmlens_start() end
 })
 vim.api.nvim_create_autocmd({"User"}, {
   pattern = {"visual_multi_exit"},
-  callback = function() require'config/vmlens'.exit() end
+  callback = function() vmlens_exit() end
 })
