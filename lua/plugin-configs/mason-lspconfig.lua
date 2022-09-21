37bad893fddba39802499fa9c641c0c40201bb5e
rydesun <rydesun@gmail.com>
2022-09-21 11:33:55 +0800

    Enable sumneko_lua telemetry

--- lua/plugin-configs/mason-lspconfig.lua
+++ lua/plugin-configs/mason-lspconfig.lua
@@ -25,7 +25,7 @@ require('mason-lspconfig').setup_handlers({
             library = vim.api.nvim_get_runtime_file("", true),
           },
           telemetry = {
-            enable = false,
+            enable = true,
           },
         }
       }
