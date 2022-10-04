8a7694043de62f5c7b9d60cd7ef7480a2ca4306e
rydesun <rydesun@gmail.com>
2022-10-04 23:36:03 +0800

    Update nvim-treesitter config

--- lua/plugin-configs/nvim-treesitter.lua
+++ lua/plugin-configs/nvim-treesitter.lua
@@ -1,50 +1,20 @@
-local ts = require("nvim-treesitter.configs")
-
-return ts.setup {
-    highlight = {
-        enable = true,
+require 'nvim-treesitter.configs'.setup {
+  highlight = { enable = true },
+  textobjects = {
+    select = {
+      enable = true,
+      -- 如果不在文本对象中，自动往下查找
+      lookahead = true,
+      keymaps = {
+        ['if'] = '@function.inner',
+        ['af'] = '@function.outer',
+        ['ic'] = '@class.inner',
+        ['ac'] = '@class.outer',
+        ['il'] = '@loop.inner',
+        ['al'] = '@loop.outer',
+        ['id'] = "@conditional.inner",
+        ['ad'] = "@conditional.outer",
+      },
     },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-        }
-    },
-    textobjects = {
-        select = {
-            enable = true,
-            -- 如果不在文本对象中，自动往下查找
-            lookahead = true,
-            keymaps = {
-                ["if"] = "@function.inner",
-                ["af"] = "@function.outer",
-                ["ic"] = "@class.inner",
-                ["ac"] = "@class.outer",
-            },
-        },
-        move = {
-            enable = true,
-            -- 在跳转列表中记录位置
-            set_jumps = true,
-            goto_next_start = {
-                ["]m"] = "@function.outer",
-                ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer"
-            },
-        },
-    },
+  },
 }
