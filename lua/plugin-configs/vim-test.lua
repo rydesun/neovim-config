8acf052bcb8e7ec7537328cacd1e02b4c230f2a0
rydesun <rydesun@gmail.com>
2022-10-07 23:16:30 +0800

    Update vim-test config

--- lua/plugin-configs/vim-test.lua
+++ lua/plugin-configs/vim-test.lua
@@ -1,5 +1,7 @@
 local function asyncrun_reuse_tab(cmd)
-  vim.api.nvim_command('AsyncRun -mode=term -pos=TAB -focus=0 -reuse ' .. cmd)
+  vim.api.nvim_command(
+    'AsyncRun -mode=term -pos=TAB -focus=0 -listed=0 -reuse '
+    .. cmd)
 end
 
 vim.g['test#custom_strategies'] = { asyncrun_reuse_tab = asyncrun_reuse_tab }
