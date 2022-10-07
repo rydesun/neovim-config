91c85db98b412a9739d514eff5b594a508051b70
rydesun <rydesun@gmail.com>
2022-10-07 22:33:29 +0800

    Update vim-test config

--- lua/plugin-configs/vim-test.lua
+++ lua/plugin-configs/vim-test.lua
@@ -1,1 +1,6 @@
-vim.g['test#strategy'] = 'asyncrun_background'
+local function asyncrun_reuse_tab(cmd)
+  vim.api.nvim_command('AsyncRun -mode=term -pos=TAB -focus=0 -reuse ' .. cmd)
+end
+
+vim.g['test#custom_strategies'] = { asyncrun_reuse_tab = asyncrun_reuse_tab }
+vim.g['test#strategy'] = 'asyncrun_reuse_tab'
