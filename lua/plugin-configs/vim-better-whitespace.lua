d5071d4bab1618ef08c80bb42a7b1bc9ce6738ad
rydesun <rydesun@gmail.com>
2022-10-08 20:57:49 +0800

    Fix whitespace in paging

--- lua/plugin-configs/vim-better-whitespace.lua
+++ lua/plugin-configs/vim-better-whitespace.lua
@@ -3,6 +3,9 @@ vim.g.better_whitespace_guicolor = 'NONE'
 -- 显示TAB前面的空格
 vim.g.show_spaces_that_precede_tabs = 1
 
+vim.g.better_whitespace_filetypes_blacklist = { 'qf', 'help', 'markdown',
+  'vim-paging' }
+
 -- 不在内置终端中显示
 vim.api.nvim_create_autocmd({ 'TermOpen' }, {
   pattern = { '*' },
--- lua/utils/term-cat.lua
+++ lua/utils/term-cat.lua
@@ -1,16 +1,17 @@
 local M = {}
 
-function M.run(utf8, replace)
-  if utf8 then
-    -- 非utf-8编码的输入自动转换
-    vim.o.fileencoding = 'utf-8'
-  end
+function M.run(to_utf8, replace)
+  -- 非utf-8编码的输入自动转换
+  if to_utf8 then vim.o.fileencoding = 'utf-8' end
+
+  -- 写入缓存文件
   local tempfile = vim.fn.tempname()
-  vim.api.nvim_command('write '..tempfile)
-  if replace then
-    vim.api.nvim_command('bdelete')
-  end
-  vim.api.nvim_command('term cat '..tempfile..'; cat')
+  vim.api.nvim_command('write ' .. tempfile)
+  if replace then vim.api.nvim_command('bdelete') end
+
+  -- 在内置终端中打开
+  vim.api.nvim_command('term cat ' .. tempfile .. '; cat')
+  vim.bo.filetype = 'vim-paging'
 end
 
 return M
