30f770e12b86e712f55cb49356c5d34e808d5ff7
rydesun <rydesun@gmail.com>
2022-10-08 17:44:10 +0800

    Update vim-better-whitespace config

--- lua/plugin-configs/vim-better-whitespace.lua
+++ lua/plugin-configs/vim-better-whitespace.lua
@@ -3,4 +3,8 @@ vim.g.better_whitespace_guicolor = 'NONE'
 -- 显示TAB前面的空格
 vim.g.show_spaces_that_precede_tabs = 1
 
-vim.g.better_whitespace_filetypes_blacklist = {'toggleterm', 'qf', 'help' }
+-- 不在内置终端中显示
+vim.api.nvim_create_autocmd({ 'TermOpen' }, {
+  pattern = { '*' },
+  callback = function() vim.api.nvim_command 'DisableWhitespace' end
+})
