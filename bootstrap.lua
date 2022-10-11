08632bafdd6ac7208a1fa26afa3cb16f649c52fb
rydesun <rydesun@gmail.com>
2022-10-11 22:26:08 +0800

    Fix bootstrap: remove compiled first

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,4 +1,5 @@
 local packer_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local packer_compiled = vim.fn.stdpath 'config' .. '/plugin/packer_compiled.lua'
 
 if vim.fn.isdirectory(packer_path) == 0 then
   vim.fn.system { 'git', 'clone', '--depth', '1',
@@ -7,4 +8,7 @@ if vim.fn.isdirectory(packer_path) == 0 then
 end
 
 require 'init'
+if vim.fn.filereadable(packer_compiled) then
+  vim.fn.delete(packer_compiled)
+end
 require 'packer'.sync()
