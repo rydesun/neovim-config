7d74454db93b08c534dc4748eb8e3a7be6481970
rydesun <rydesun@gmail.com>
2023-03-10 16:13:55 +0800

    Update obsidian commands

--- lua/plugins/configs/obsidian.lua
+++ lua/plugins/configs/obsidian.lua
@@ -1,4 +1,5 @@
 local default_dir = vim.fn.expand('~/Data/Documents/Obsidian Vault')
+local diary_dir = default_dir .. '/日记'
 
 vim.api.nvim_create_user_command(
   'NvimTreeObsidian', 'NvimTreeFindFileToggle ' .. default_dir, {}
@@ -14,11 +15,21 @@ vim.api.nvim_create_user_command(
 )
 
 vim.api.nvim_create_user_command(
+  'ObsidianDiary',
+  function()
+    name = vim.fn.strftime '%F'
+    path = string.format('%s/%s.md', diary_dir, name)
+    vim.cmd.edit(path)
+  end, {}
+)
+
+vim.api.nvim_create_user_command(
   'ObsidianSync', function()
     msg = 'vault backup: ' .. vim.fn.strftime '%F %T'
     vim.api.nvim_command(string.format(
-      "!cd '%s' && git add . && git commit -m '%s'; git pull -r && git push",
-      default_dir, msg))
+      "!cd '%s' && git add . && git commit -m '%s'", default_dir, msg))
+    vim.api.nvim_command(string.format(
+      "!cd '%s' && git pull -r && git push", default_dir, msg))
   end, {}
 )
 
