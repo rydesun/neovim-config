d89897febcad7d5a05c1ddc71cd1d360d966828a
rydesun <rydesun@gmail.com>
2023-05-10 01:48:56 +0800

    Refactor colorscheme

--- lua/plugins/configs/everforest.lua
+++ lua/plugins/configs/everforest.lua
@@ -6,57 +6,55 @@ vim.g.everforest_background = 'hard'
 vim.g.everforest_disable_italic_comment = 1
 
 local function everforest_custom()
+  local hi = vim.fn['everforest#highlight']
   -- 在更改background选项后，必须重新获取palette
   local palette = vim.fn['everforest#get_palette'](vim.g.everforest_background,
     vim.fn['everforest#get_configuration']().colors_override)
 
   -- 浮动窗口
-  vim.fn['everforest#highlight']('FloatBorder', palette.bg4, palette.none)
+  hi('FloatBorder', palette.bg4, palette.none)
 
   -- cursor
-  vim.fn['everforest#highlight']('CursorLine', palette.none, palette.bg0)
+  hi('CursorLine', palette.none, palette.bg0)
 
   -- winbar
-  vim.fn['everforest#highlight']('WinBar', palette.none, palette.bg_blue)
-  vim.fn['everforest#highlight']('WinBarNC', palette.none, palette.bg_blue)
+  hi('WinBar', palette.none, palette.bg_blue)
+  hi('WinBarNC', palette.none, palette.bg_blue)
 
   -- tabline
-  vim.cmd.hi('clear TabLineFill')
+  vim.cmd 'hi clear TabLineFill'
 
   -- virtual text
-  vim.fn['everforest#highlight'](
-    'VirtualTextError', palette.grey0, palette.bg_red)
-  vim.fn['everforest#highlight'](
-    'VirtualTextWarning', palette.grey0, palette.bg_yellow)
-  for _, name in pairs { 'VirtualTextInfo', 'VirtualTextHint' } do
-    vim.fn['everforest#highlight'](name, palette.grey0, palette.bg_green)
-  end
+  local virtual_text = {
+    { 'VirtualTextError',   palette.bg_red },
+    { 'VirtualTextWarning', palette.bg_yellow },
+    { 'VirtualTextInfo',    palette.bg_green },
+    { 'VirtualTextHint',    palette.bg_green },
+  }
+  for _, zip in pairs(virtual_text) do hi(zip[1], palette.grey0, zip[2]) end
 
   -- InlayHint
-  vim.fn['everforest#highlight']('InlayHint', palette.grey0, palette.bg2)
+  hi('InlayHint', palette.grey0, palette.bg2)
 
   -- 折叠行
-  vim.fn['everforest#highlight']('Folded', palette.aqua, palette.bg_blue)
+  hi('Folded', palette.aqua, palette.bg_blue)
 
   -- nvim-cmp
-  vim.fn['everforest#highlight']('PmenuSel', palette.none, palette.bg_visual)
+  hi('PmenuSel', palette.none, palette.bg_visual)
 
   -- vim-better-whitespace
-  vim.fn['everforest#highlight']('ExtraWhitespace',
-    palette.none, palette.bg_red)
+  hi('ExtraWhitespace', palette.none, palette.bg_red)
 
   -- leap.nvim
-  vim.api.nvim_command('hi! link LeapLabelPrimary Search')
-  vim.api.nvim_command('hi! link LeapLabelSecondary DiffText')
+  vim.cmd 'hi link LeapLabelPrimary Search'
+  vim.cmd 'hi link LeapLabelSecondary DiffText'
 
   -- sniprun
-  vim.api.nvim_command('hi! link SniprunVirtualTextOk VirtualTextInfo')
-  vim.api.nvim_command('hi! link SniprunVirtualTextErr VirtualTextInfo')
+  vim.cmd 'hi link SniprunVirtualTextOk VirtualTextInfo'
+  vim.cmd 'hi link SniprunVirtualTextErr VirtualTextInfo'
 
   -- 分页时
-  if vim.g.paging then
-    vim.fn['everforest#highlight']('MsgArea', palette.none, palette.bg2)
-  end
+  if vim.g.paging then hi('MsgArea', palette.none, palette.bg2) end
 end
 
 vim.api.nvim_create_autocmd('ColorScheme', {
@@ -64,4 +62,4 @@ vim.api.nvim_create_autocmd('ColorScheme', {
   callback = everforest_custom,
 })
 
-vim.api.nvim_command('colorscheme everforest')
+vim.cmd 'colorscheme everforest'
