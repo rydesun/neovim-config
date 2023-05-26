ead154637b9806c78e38144a4fdc64ac8e5b1961
rydesun <rydesun@gmail.com>
2023-05-26 23:06:15 +0800

    Update mini.ai config

--- lua/plugins/configs/mini-ai.lua
+++ lua/plugins/configs/mini-ai.lua
@@ -1,6 +1,9 @@
 local spec_treesitter = require 'mini.ai'.gen_spec.treesitter
 
 return {
+  -- 50也太少了
+  n_lines = 1000,
+
   -- f a n l 已被占用
   custom_textobjects = {
     c = spec_treesitter { a = '@comment.outer', i = '@comment.inner' },
@@ -8,8 +11,8 @@ return {
     v = spec_treesitter { a = '@assignment.rhs', i = '@assignment.rhs' },
     d = spec_treesitter { a = '@conditional.outer', i = '@conditional.inner' },
     o = spec_treesitter { a = '@loop.outer', i = '@loop.inner' },
-    e = spec_treesitter { a = '@return.outer', i = '@return.inner' },
-
+    e = spec_treesitter { a = '@call.outer', i = '@call.inner' },
+    E = spec_treesitter { a = '@return.outer', i = '@return.inner' },
     C = spec_treesitter { a = '@class.outer', i = '@class.inner' },
     F = spec_treesitter { a = '@function.outer', i = '@function.inner' },
   }
