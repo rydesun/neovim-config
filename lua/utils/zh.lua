ec66784b443f22afdc238b13afd29b8b64493df0
rydesun <rydesun@gmail.com>
2023-08-05 21:02:22 +0800

    Update zh space plugin

--- lua/utils/zh.lua
+++ lua/utils/zh.lua
@@ -1,16 +1,17 @@
-local M = { }
+local M = {}
 
-local hans = '[\\u3400-\\u4DBF\\u4E00-\\u9FFF\\uF900-\\uFAFF\\U20000-\\U323AF]'
-local alnum = '[a-zA-Z0-9]'
+local han = '[\\u3400-\\u4DBF\\u4E00-\\u9FFF\\uF900-\\uFAFF\\U20000-\\U323AF]'
+local latin = '[a-zA-Z0-9\'"$&`]'
 
 function M.count()
-  vim.cmd('%s/' .. hans .. '//gn')
+  vim.cmd('%s/' .. han .. '//gn')
 end
 
 function M.typo_space()
   local cmd = [[silent! %s/\(%s\)\(%s\)/\1 \2/g]]
-  vim.cmd(cmd:format('%s', hans, alnum))
-  vim.cmd(cmd:format('%s', alnum, hans))
+  vim.cmd(cmd:format('%s', han, latin))
+  vim.cmd(cmd:format('%s', latin, han))
+  vim.cmd.nohlsearch()
 end
 
 return M
