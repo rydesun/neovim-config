88b1d595040f78e6292f3dd7160898b11e7dc3cc
rydesun <rydesun@gmail.com>
2025-02-12 18:32:51 +0800

    Update git staged signs

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -1,11 +1,14 @@
+local signs = {
+  add = { text = '+│' },
+  change = { text = '~│' },
+  delete = { text = '_│' },
+  topdelete = { text = '‾│' },
+  changedelete = { text = '≈│' },
+  -- 不提示未追踪的文件
+  untracked = { text = '' },
+}
+
 return {
-  signs = {
-    add = { text = '+│' },
-    change = { text = '~│' },
-    delete = { text = '_│' },
-    topdelete = { text = '‾│' },
-    changedelete = { text = '≃│' },
-    -- 不提示未追踪的文件
-    untracked = { text = '' },
-  },
+  signs = signs,
+  signs_staged = signs,
 }
