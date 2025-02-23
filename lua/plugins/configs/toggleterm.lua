e095b1da4c9b247852fff407be42621497e1c0af
rydesun <rydesun@gmail.com>
2025-02-23 20:44:50 +0800

    Update toggleterm config

--- lua/plugins/configs/toggleterm.lua
+++ lua/plugins/configs/toggleterm.lua
@@ -4,7 +4,10 @@ return {
   -- 不要变暗
   shade_terminals = false,
 
-  -- 已经配置TermOpen事件关闭number column (opt绑定到buffer)
-  -- 所以不用插件的方式关闭(该插件绑定opt到win)
-  hide_numbers = false,
+  float_opts = {
+    border = require 'libs.style'.border(),
+  },
+  highlights = {
+    FloatBorder = { link = 'FloatBorder' },
+  },
 }
