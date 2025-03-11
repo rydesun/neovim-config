4b97668a76f1f5dcac55e62674fffc523ff7d72b
rydesun <rydesun@gmail.com>
2025-03-11 10:54:44 +0800

    Fix lsp config

--- lua/plugins/configs/mason-lspconfig.lua
+++ lua/plugins/configs/mason-lspconfig.lua
@@ -1,16 +1,16 @@
 local lspconfig = require 'lspconfig'
+
 local capabilities = require 'cmp_nvim_lsp'.default_capabilities()
 capabilities.textDocument.foldingRange = {
   dynamicRegistration = false, lineFoldingOnly = true,
 }
 
 local handlers = {
-  function(server_name)
-    lspconfig[server_name].setup { capabilities = capabilities }
-  end
+  -- mason安装的ls如果没有单独配置，则使用此配置
+  function(name) lspconfig[name].setup { capabilities = capabilities } end
 }
 
-handlers.lua_ls = function()
+handlers.lua_ls = function(name)
   local settings = {}
   settings.Lua = {
     runtime = { version = 'LuaJIT' },
@@ -22,39 +22,36 @@ handlers.lua_ls = function()
       },
     },
   }
-  lspconfig.lua_ls.setup {
-    capabilities = capabilities,
-    settings = settings,
-  }
+  lspconfig[name].setup { capabilities = capabilities, settings = settings }
 end
 
-handlers.jsonls = function()
-  capabilities.documentFormattingProvider = false
+handlers.jsonls = function(name)
   local settings = {}
   settings.json = {
     schemas = require 'schemastore'.json.schemas(),
     validate = { enable = true },
   }
-  lspconfig.jsonls.setup {
+  lspconfig[name].setup {
     capabilities = capabilities,
     settings = settings,
+    on_attach = function(client)
+      -- 让prettier来格式化
+      client.server_capabilities.documentFormattingProvider = false
+    end,
   }
 end
 
-handlers.yamlls = function()
+handlers.yamlls = function(name)
   local settings = {}
   settings.yaml = {
     schemaStore = { enable = false, url = '' },
     schemas = require 'schemastore'.yaml.schemas(),
   }
-  lspconfig.yamlls.setup {
-    capabilities = capabilities,
-    settings = settings,
-  }
+  lspconfig[name].setup { capabilities = capabilities, settings = settings }
 end
 
-handlers.cssls = function()
-  lspconfig.cssls.setup {
+handlers.cssls = function(name)
+  lspconfig[name].setup {
     capabilities = capabilities,
     init_options = { provideFormatter = false },
   }
