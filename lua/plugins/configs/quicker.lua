4090d8bcf1e5875948f8b854f3352d414457c875
rydesun <rydesun@gmail.com>
2025-03-15 00:18:14 +0800

    Add quickfix keymaps

--- lua/plugins/configs/quicker.lua
+++ lua/plugins/configs/quicker.lua
@@ -7,4 +7,27 @@ return {
     soft_cross = '┼',
     soft_end = '┤',
   },
+  keys = {
+    {
+      '<leader>j',
+      function()
+        require 'quicker'.expand {
+          before = 0,
+          after = 1,
+          add_to_existing = true,
+        }
+      end,
+    },
+    {
+      '<leader>k',
+      function()
+        require 'quicker'.expand {
+          before = 1,
+          after = 0,
+          add_to_existing = true,
+        }
+      end,
+    },
+    { '<leader>s', function() require 'quicker'.collapse() end },
+  },
 }
