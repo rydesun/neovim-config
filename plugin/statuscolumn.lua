9ae8153df81910d44bd2c0fdb3394b4ca697f4dc
rydesun <rydesun@gmail.com>
2025-04-06 23:06:34 +0800

    Simulate signcolumn=number-auto

--- plugin/statuscolumn.lua
+++ plugin/statuscolumn.lua
@@ -6,6 +6,9 @@ local M = {
   cursor_char = '➜',
   cursor_padding_char = ' ',
   number_padding_char = '0',
+
+  -- signcolumn=number时，允许signs动态增加列
+  signcolumn_number_auto = 2,
 }
 
 vim.o.statuscolumn = '%{%v:lua.StatusColumn()%}'
@@ -18,6 +21,7 @@ function StatusColumn()
   return text
 end
 
+---@return string?
 function M.number_signcolumn()
   -- 填充虚拟行
   if vim.v.virtnum > 0 then return M.wrap_fillchar end
@@ -29,13 +33,20 @@ function M.number_signcolumn()
   -- 非折叠行显示其他符号
   if #signs > 0 then
     local with_number = vim.wo.number or vim.wo.relativenumber
-    if vim.wo.signcolumn == 'number' and with_number then return '%l' end
+    if vim.wo.signcolumn == 'number' and with_number then
+      -- 手动拼接signs
+      local signs_text = M.format_signs(signs, M.signcolumn_number_auto)
+      -- TODO: 什么情况下有sign无sign_text？
+      if signs_text ~= '' then return signs_text end
+    end
+    -- fallback: 交给nvim处理
     local number = with_number and M.numbercolumn() or ''
     return '%s' .. number
   end
   return M.numbercolumn()
 end
 
+---@return string?
 function M.numbercolumn()
   -- 带填充的相对行号
   if vim.wo.relativenumber then
@@ -57,19 +68,50 @@ function M.numbercolumn()
   if vim.wo.number then return '%l' end
 end
 
+---@param lnum integer
+---@return string?, vim.api.keyset.get_extmark_item[]
 function M.get_fold_signs(lnum)
-  local signs = M._get_signs(lnum)
+  local signs = M.get_signs(lnum)
   if vim.fn.foldclosed(lnum) ~= lnum then
     return nil, signs
   end
   if #signs > 0 then return M.fold_sign_nested, signs end
   for i = lnum + 1, vim.fn.foldclosedend(lnum) do
-    if #M._get_signs(i) > 0 then return M.fold_sign_nested, signs end
+    if #M.get_signs(i) > 0 then return M.fold_sign_nested, signs end
   end
   return M.fold_sign, signs
 end
 
-function M._get_signs(lnum)
-  return vim.fn.sign_getplaced(vim.fn.bufnr(),
-    { group = '*', lnum = lnum })[1].signs
+---@param lnum integer
+---@return vim.api.keyset.get_extmark_item[]
+function M.get_signs(lnum)
+  local row = lnum - 1
+  return vim.api.nvim_buf_get_extmarks(0, -1, { row, 0 }, { row, -1 },
+    { details = true, type = 'sign' })
+end
+
+local sign_fmt = '%%#%s#%s'
+---@param signs vim.api.keyset.get_extmark_item[]
+---@param auto_signs integer 选取最高优先级的个数
+---@return string
+function M.format_signs(signs, auto_signs)
+  if auto_signs > 0 then
+    table.sort(signs, function(a, b) return a[4].priority > b[4].priority end)
+    return vim.iter(signs):take(auto_signs):map(function(sign)
+      local sign_text = sign[4].sign_text
+      if not sign_text then return '' end
+      return sign_fmt:format(sign[4].sign_hl_group, sign_text)
+    end):join ''
+  end
+  -- 只取优先级最高的sign
+  local found_sign
+  for _, sign in ipairs(signs) do
+    if sign[4].sign_text and (
+          not found_sign or sign[4].priority > found_sign[4].priority) then
+      found_sign = sign
+    end
+  end
+  return found_sign and sign_fmt:format(
+    found_sign[4].sign_hl_group, found_sign[4].sign_text
+  ) or ''
 end
