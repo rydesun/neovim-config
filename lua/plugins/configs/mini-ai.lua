2c3479e2910a32c221222fe5c1c7620dd2d1265d
rydesun <rydesun@gmail.com>
2026-06-15 20:42:09 +0800

    0.12 keybindings

--- lua/plugins/configs/mini-ai.lua
+++ lua/plugins/configs/mini-ai.lua
@@ -6,7 +6,6 @@ return {
 
   -- 出现在a和i后面的字母保留情况：ab hiI l n pq sSt wW
   -- 出现在a和i后面的按键被占用的情况：
-  -- n和l被占用：在mini.ai自定义的文本对象前面：n=next, l=last
   -- ii和ai被snacks.scope占用
   -- 在keymaps.vim手动设置nvim-various-textobjs
   -- 字母abq、空格、数字、标点符号，以及其他见h: MiniAi-textobject-builtin
@@ -24,5 +23,12 @@ return {
     B = spec_treesitter { a = '@block.outer', i = '@block.inner' },
     C = spec_treesitter { a = '@class.outer', i = '@class.inner' },
     F = spec_treesitter { a = '@function.outer', i = '@function.inner' },
+  },
+  -- 防止与Nvim 0.12 增量选择冲突
+  mappings = {
+    around_next = 'aN',
+    inside_next = 'iN',
+    around_last = 'aL',
+    inside_last = 'iL',
   },
 }
